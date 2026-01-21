70be99644973d130f106d5d8c4aaf00f55b17dbc
Cassius269 <fahamygaston@gmail.com>
2026-01-21 11:35:24 +0100

    création de la fonctionnalité de recherche de recettes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Recipe from './components/Recipe';
 import Recipes from './components/Recipes';
+import SearchBar from './components/SearchBar';
 
 export default function App(){
     return(
         <>
             <Header />
             <main className='container'>
+                <SearchBar />
                 <Recipes />
             </main>
             <Footer />

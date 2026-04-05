907e5f80749ca3299517aa9967ff5938d0c86bc1
cassius269 <fahamygaston@gmail.com>
2026-04-05 23:33:36 +0200

    ajout d'une fonction de clean-up

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -10,26 +10,35 @@ function Homepage() {
 
   // Récupérer les recettes de l'API après le rendu composant Homepage
   useEffect(() => {
+    let cancel = false;
     const getRecipesFromAPI = async () => {
       try {
         setIsLoading(true);
         const response = await fetch("https://www.restapi.fr/api/recipes");
 
-        if (response.ok) {
+        // si la réponse est ok et que la requête n'est pas annulée
+        if (response.ok && !cancel) {
           const data = await response.json();
           console.log(data);
-          setRecipes(data);
+          setRecipes(Array.isArray(data) ? data : [data]); // si c'est la donnée est un tableau on met dans l'état des recettes directement sinonen créer un nouveau tableau
         } else {
           console.log("Ooops, une erreur");
         }
       } catch (error) {
         console.log(`Erreur: ${error.message}`);
       } finally {
-        setIsLoading(false);
+        // si la requête est en cours, mettre le loading à false
+        if (!cancel) {
+          setIsLoading(false);
+        }
       }
     };
 
+    // Appel de la fonction de récupération des recettes depuis l'API
     getRecipesFromAPI();
+
+    // Fonction de clean-up
+    return () => (cancel = true);
   }, []);
 
   // console.log(recipes);
